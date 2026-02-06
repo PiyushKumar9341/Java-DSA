141532b00cba6808029b2a4d76dce61c5cee0014
Piyush Kumar <piyush.s9341@gmail.com>
2026-02-06 21:11:39 +0530

    time complexity

--- Monkey Dsa.java/pattern10.java
+++ Monkey Dsa.java/pattern10.java
@@ -5,7 +5,7 @@ public class pattern10 {
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
                 
-                // If the sum of row index (i) and column index (j) is even,
+                // If the sum of row index (i) and column index (j) is even.
                 // it follows the pattern shown in the image.
                 if ((i + j) % 2 == 0) {
                     System.out.print("1 ");
