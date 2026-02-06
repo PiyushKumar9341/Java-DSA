fd937170c3d8f64273860ac8114d6992d4f30c36
Piyush Kumar <piyush.s9341@gmail.com>
2026-02-06 21:15:58 +0530

    time complexity

--- Monkey Dsa.java/pattern10.java
+++ Monkey Dsa.java/pattern10.java
@@ -1,6 +1,6 @@
 public class pattern10 {
     public static void main(String[] args) {
-        int n = 5; // Number of rows
+        int n = 5; // Number of rows.
 
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
