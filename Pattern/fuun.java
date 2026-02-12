91eb96181481995ff49fdf8d3f4dae8e9689de7b
Piyush Kumar <piyush.s9341@gmail.com>
2026-02-12 10:35:53 +0530

    problem solving

--- Pattern/fuun.java
+++ Pattern/fuun.java
@@ -6,7 +6,7 @@ public class fuun {
                 System.out.println(name);
                 return;
                 
-              }    
+              }
 
               public static void main(String[] args) {
                 Scanner sc = new Scanner(System.in);
