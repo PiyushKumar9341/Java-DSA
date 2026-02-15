ceadff34526c1b7e958dd266a5e95c0b174352bb
Piyush Kumar <piyush.s9341@gmail.com>
2026-02-15 23:33:54 +0530

    new codes

---
+++ Pattern/pat4.java
@@ -0,0 +1,18 @@
+
+public class pat4 { 
+
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <n-i+1 ; j++) {
+
+                System.out.print("* ");
+            }
+
+            System.out.println("");
+        
+            
+        }
+        
+    }
+}
