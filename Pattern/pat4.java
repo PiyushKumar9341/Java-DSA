2724223b66d004980e615b25ea558974ec6e703f
Piyush Kumar <piyush.s9341@gmail.com>
2026-02-19 12:24:33 +0530

    changes

--- Pattern/pat4.java
+++ Pattern/pat4.java
@@ -4,7 +4,7 @@ public class pat4 {
     public static void main(String[] args) {
         int n = 5;
         for (int i = 1; i <= n; i++) {
-            for (int j = 0; j <n-i+1 ; j++) {
+            for (int j = 0; j <n-i+1; j++) {
 
                 System.out.print("* ");
             }
